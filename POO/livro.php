5514d9f10395ab1b10878aeef924cd187d492f51
fortunatogio <161900831+fortunatogio@users.noreply.github.com>
2024-08-19 21:56:56 -0300

    gio

--- POO/livro.php
+++ POO/livro.php
@@ -9,21 +9,21 @@
         public function __construct($titulo,$autor,$ano){
             $this->titulo = $titulo;
             $this->autor = $autor;
-            $this->setAno(ano);
+            $this->setAno($ano);
         }
 
         public function setTitulo($titulo){
             $this->titulo = $titulo;
         }
         public function getTitulo($titulo){
-            return $this->titulo
+            return $this->titulo;
         }
         
         public function setAutor($autor){
             $this->autor = $autor;
         }
         public function getAutor($autor){
-            return $this->autor
+            return $this->autor;
         }
 
         public function setAno($ano){
