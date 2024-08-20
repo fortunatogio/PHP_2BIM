2a9680be440f70952160aa4e063ac3d72a03721f
fortunatogio <161900831+fortunatogio@users.noreply.github.com>
2024-08-19 21:25:36 -0300

    gio

---
+++ POO/livro.php
@@ -0,0 +1,39 @@
+<?php
+
+    class Livro {
+
+        private $titulo;
+        private $autor;
+        private $ano;
+
+        public function __construct($titulo,$autor,$ano){
+            $this->titulo = $titulo;
+            $this->autor = $autor;
+            $this->setAno(ano);
+        }
+
+        public function setTitulo($titulo){
+            $this->titulo = $titulo;
+        }
+        public function getTitulo($titulo){
+            return $this->titulo
+        }
+        
+        public function setAutor($autor){
+            $this->autor = $autor;
+        }
+        public function getAutor($autor){
+            return $this->autor
+        }
+
+        public function setAno($ano){
+            if ($ano < 1900)
+                $ano = 1900;
+            $this->ano = $ano;
+        }
+        public function getAno($ano){
+            return $this->ano;
+        }
+    }
+
+$obj = new Livro("O livro", "Ana", "2024");
